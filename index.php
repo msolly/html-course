7cb6c258ce87c3069e19b13a4ff20668bd0a4d42
msolly <msolivie@gmail.com>
2020-12-25 18:01:23 +0100

    changed index to php

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<html>
+	<head>
+		<title>Olivia Scuderoni | Resume</title>
+
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+
+<nav class="navbar navbar-default">
+  <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Olivia Scuderoni</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">    
+        
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="personal_statement.html">Personal Statement</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+
+	<!-- Top Resume Info -->
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><strong>Service Desk Engineer</strong></h3>
+  </div>
+  <div class="panel-body">
+
+     
+     <div class="col-md-8">
+     	<strong>Service Desk Engineer</strong>
+     	<br>
+<p align="justify">handled first line incidents and requests
+• Cisco/ Juniper troubleshooting
+• conducted managerial and technical escalations
+• ensured compliance with service operations
+processes and metrics
+• interfaced with external providers to ensure
+service level agreements</p>
+ <br>
+ </div>   	
+ <div class="col-md-4" align="right">
+ <em>2016 - 2018</em>
+</div>
+
+   <div class="col-md-8">
+     	<strong>Service Desk Engineer</strong>
+     	<br>
+<p align="justify">handled first line incidents and requests
+• Cisco/ Juniper troubleshooting
+• conducted managerial and technical escalations
+• ensured compliance with service operations
+processes and metrics
+• interfaced with external providers to ensure
+service level agreements</p>
+ <br>
+ </div>   	
+ <div class="col-md-4" align="right">
+ <em>2016 - 2018</em>
+</div>
+
+  </div>
+</div>
+<br>
+<br>
+
+<!-- Technical Skills section -->
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><strong>Technical Skills</strong></h3>
+  </div>
+  <div class="panel-body">
+    <div class="col-md-3">Stuff</div>
+    <div class="col-md-3">Stuff</div>
+    <div class="col-md-3">Stuff</div>
+  </div>
+</div>
+
+<br>
+<br>
+
+<!-- Education -->
+
+<div class="panel panel-default">
+  <div class="panel-heading">
+    <h3 class="panel-title"><strong>Education</strong></h3>
+  </div>
+  <div class="panel-body">
+
+    <div class="col-md-2"><img src="image/bradford.png" height="50"></div>
+   
+
+    <p align="justify"> <strong>University of Bradford</strong>, Bradford, UK<br>
+    M.A. in International Development Management</p><br>
+<br>
+<br>
+
+<div class="col-md-2"><img src="image/Sapienza.png" height="50"></div>
+
+
+	<p align="justify"> <strong>University La Sapienza</strong>, Rome, Italy<br>
+ 	B.A. in Foreign Languages and Literatures</p><br>
+
+
+    
+  </div>
+</div>
+
+
+<br>
+<br>
+	<p align="center">&copy; MS OLLY - All rights reserved</p>
+	<br>
+</div>
+
+</body>
+</html>
